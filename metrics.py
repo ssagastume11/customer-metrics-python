a9d0868bc21b3770a4aa52ff2da0e6812c820b7a
ssagastume11 <sagassergio123@gmail.com>
2026-06-01 14:33:35 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,2 +1,43 @@
 def get_customer_metrics(*, data, from_, to, min_total_spend=None):
-    raise NotImplementedError("Not implemented")
+    customers = {}
+
+    for order in data:
+        order_date = order["orderDate"]
+
+        # Inclusive date filter
+        if order_date < from_ or order_date > to:
+            continue
+
+        customer_id = order["customerId"]
+
+        order_amount = sum(
+            item["quantity"] * item["unitPrice"]
+            for item in order["lineItems"]
+        )
+
+        if customer_id not in customers:
+            customers[customer_id] = {
+                "customerId": customer_id,
+                "orderCount": 0,
+                "totalSpend": 0,
+            }
+
+        customers[customer_id]["orderCount"] += 1
+        customers[customer_id]["totalSpend"] += order_amount
+
+    results = []
+
+    for customer in customers.values():
+        customer["avgOrderValue"] = (
+            customer["totalSpend"] / customer["orderCount"]
+        )
+
+        if (
+            min_total_spend is None
+            or customer["totalSpend"] >= min_total_spend
+        ):
+            results.append(customer)
+
+    results.sort(key=lambda x: x["customerId"])
+
+    return {"results": results}
